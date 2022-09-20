f63ab2f32e9d6a530e5d2ed8835bafed800d3ecc
valkyrie_pilot <valk@randomairborne.dev>
2022-09-19 23:09:03 -0600

    Finish everything about circles

--- everything-about-a-circle.rs
+++ everything-about-a-circle.rs
@@ -1,16 +1,36 @@
 use std::f64::consts::PI;
 
 fn main() {
+    let mut kind = String::new();
+    println!("Do you have the radius, circumference, diameter, or area of the circle?");
+    std::io::stdin().read_line(&mut kind).unwrap();
+    if kind.starts_with('r') {
+        println!("Please input the radius of your circle.");
+    } else if kind.starts_with('c') {
+        println!("Please input the circumference of your circle.");
+    } else if kind.starts_with('d') {
+        println!("Please input the diameter of your circle.");
+    } else if kind.starts_with('a') {
+        println!("Please input the area of your circle.");
+    } else {
+        println!("Please input radius, circumference, diameter, or area.");
+        main();
+    }
     let mut line = String::new();
-    println!("Please input the radius of your circle");
     std::io::stdin().read_line(&mut line).unwrap();
-    let radius: f64 = line.trim().parse().unwrap();
-    println!(
-        "diameter: {} units, circumference: {} units, area: {} square units",
-        radius * 2.0,
-        radius * 2.0 * PI,
-        PI * radius.powf(2.0)
-    );
+    let input: f64 = line.trim().parse().unwrap();
+    if kind.starts_with('r') {
+        println!("{}", Circle::from_radius(input));
+    } else if kind.starts_with('c') {
+        println!("{}", Circle::from_circumference(input));
+    } else if kind.starts_with('d') {
+        println!("{}", Circle::from_diameter(input));
+    } else if kind.starts_with('a') {
+        println!("{}", Circle::from_area(input));
+    } else {
+        println!("Please input radius, circumference, diameter, or area.");
+        main();
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -26,14 +46,14 @@ impl Circle {
     }
 
     fn from_diameter(diameter: f64) -> Circle {
-        Circle::from_radius(diameter/2)
+        Circle::from_radius(diameter / 2.0)
     }
 
     fn from_circumference(circ: f64) -> Circle {
         Circle::from_radius(circ / PI)
     }
 
-    fn from_radius(radius: f64) {
+    fn from_radius(radius: f64) -> Circle {
         Circle {
             radius,
             diameter: radius * 2.0,
@@ -56,7 +76,7 @@ impl Circle {
 }
 
 impl std::fmt::Display for Circle {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(
             f,
             "radius: {}, diameter: {}, area: {}, circumference: {}",
