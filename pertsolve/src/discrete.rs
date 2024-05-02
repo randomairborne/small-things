e810974d6a504971dbb11d279f59fb63d804725d
valkyrie_pilot <valk@randomairborne.dev>
2024-05-02 10:49:06 -0600

    Discrete improvements

--- pertsolve/src/discrete.rs
+++ pertsolve/src/discrete.rs
@@ -1,4 +1,7 @@
+pub fn find_a(principal: f64, rate: f64, time: f64) -> f64 {
+    principal * (1.0 + rate).powf(time)
+}
 
-pub fn find_a(principal: f64, rate: f64, time: i64) -> f64 {
-    principal * ((rate + 100)/100).powi(time)
+pub fn effective_rate(rate: f64, period: f64) -> f64 {
+    (1.0 + (rate / period)).powf(period) - 1.0
 }
--- pertsolve/src/main.rs
+++ pertsolve/src/main.rs
@@ -1,13 +1,8 @@
-use pertsolve::{continuous};
+use pertsolve::{continuous, discrete};
 use std::io::Write;
 
 fn main() {
-    let output = continuous_compute();
-    println!("{input} = {output}");
-}
-
-fn continuous_compute() -> f64 {
-    eprint!("Pick the a = Pe^(rt) value (or h) you wish to compute: ");
+    eprint!("Do you wish to compute continuously or discretely? ");
     std::io::stderr().flush().unwrap();
     let mut input = String::new();
     std::io::stdin()
@@ -15,6 +10,24 @@ fn continuous_compute() -> f64 {
         .expect("Failed to read stdin");
     let input = input.trim();
     match input.to_ascii_uppercase().trim() {
+        "C" | "CONTINUOUS" => continuous_compute(),
+        "D" | "DISCRETE" => discrete_compute(),
+        _ => {
+            eprintln!("Invalid choice");
+            return;
+        }
+    };
+}
+
+fn continuous_compute() {
+    eprint!("Pick the A = Pe^(rt) value (or h) you wish to compute: ");
+    std::io::stderr().flush().unwrap();
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read stdin");
+    let input = input.trim();
+    let output = match input.to_ascii_uppercase().trim() {
         "A" => continuous::find_a(get_float("principal"), get_float("rate"), get_float("time")),
         "T" => continuous::find_t(
             get_float("amount wanted"),
@@ -31,7 +44,44 @@ fn continuous_compute() -> f64 {
             eprintln!("Invalid choice");
             f64::NAN
         }
-    }
+    };
+
+    println!("{input} = {output}");
+}
+
+fn discrete_compute() {
+    eprint!("Pick the A = (P * (1 + r)^t) - 1 value you wish to compute: ");
+    std::io::stderr().flush().unwrap();
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read stdin");
+    let input = input.trim();
+    let output = match input.to_ascii_uppercase().trim() {
+        "A" => continuous::find_a(get_float("principal"), get_float("rate"), get_float("time")),
+        "T" => {
+            let y_rate = get_float("rate");
+            let c_per_rate = get_float("compounds per rate");
+            let r_effective = discrete::effective_rate(y_rate, c_per_rate);
+            continuous::find_t(
+                get_float("amount wanted"),
+                get_float("principal"),
+                r_effective,
+            )
+        }
+        "R" => continuous::find_r(
+            get_float("amount wanted"),
+            get_float("principal"),
+            get_float("time"),
+        ),
+        "H" | "HL" | "HALF LIFE" => continuous::find_r_hl(get_float("half life")),
+        _ => {
+            eprintln!("Invalid choice");
+            f64::NAN
+        }
+    };
+
+    println!("{input} = {output}");
 }
 
 fn get_float(msg: &str) -> f64 {
