f32641edc1cd145f33bfd61999ddebb91fb69c03
valkyrie_pilot <valk@randomairborne.dev>
2024-08-17 12:04:59 -0600

    Drier wiggleworm.rs

--- individuals/src/bin/wiggleworm.rs
+++ individuals/src/bin/wiggleworm.rs
@@ -1,23 +1,20 @@
 use std::thread::sleep;
 use std::time::Duration;
 const TIME: Duration = Duration::from_millis(50);
+const DISTANCE: usize = 10;
 fn main() {
+    let mut counter = 0;
+    let mut inc = true;
     loop {
         sleep(TIME);
-        println!("wiggle");
-        sleep(TIME);
-        println!(" wiggle");
-        sleep(TIME);
-        println!("  wiggle");
-        sleep(TIME);
-        println!("   wiggle");
-        sleep(TIME);
-        println!("   wiggle");
-        sleep(TIME);
-        println!("  wiggle");
-        sleep(TIME);
-        println!(" wiggle");
-        sleep(TIME);
-        println!("wiggle");
+        println!("{:<counter$}wiggle", "");
+        if inc {
+            counter += 1;
+        } else {
+            counter -= 1;
+        }
+        if counter % DISTANCE == 0 {
+            inc = !inc;
+        }
     }
 }
